5b66d22bed792647b47d2b0b2d3fe5c7c2ed72c3
Emilio Zapata <124ezapata@frhsd.com>
2023-11-28 11:00:38 -0500

    fixed java

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
 let number = Math.ceil(Math.random() * 1000);
+let display = getElementById("display");
 
 btn.onclick = function () {
   container.style.transform = "rotate(" + number + "deg)";
